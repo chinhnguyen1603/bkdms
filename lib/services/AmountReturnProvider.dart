f7a6007984ab9047dba2a3abbf29718f88104212
Nguyễn Ngọc Chính <67193154+chinhnguyen1603@users.noreply.github.com>
2022-04-30 18:25:25 +0700

    update sumOfOrder returnOrder

---
+++ lib/services/AmountReturnProvider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class AmountReturnProvider with ChangeNotifier{
+  List<String> lstAmount = [];
+  int sumOfOrder = 0;
+
+  void initList (int index, String amount){
+    this.lstAmount.insert(index, amount);
+    notifyListeners();
+  }
+  void setNewAmount(int index, String newAmount){
+    this.lstAmount[index] = newAmount;
+    notifyListeners();
+  }
+  void removeIndex(int index){
+    this.lstAmount.removeAt(index);
+    notifyListeners();
+  }
+  void clearList(){
+    this.lstAmount.clear();
+    notifyListeners();
+  }
+
+}
